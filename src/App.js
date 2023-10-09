361d02084aced4796f908e269434f3107ad62ff9
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-08 20:02:50 -0500

    added Github Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
+          Darcy Weipert
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Darcy-Weipert/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
