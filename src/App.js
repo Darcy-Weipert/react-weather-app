034561dadbe68be21cd84bdc27a74f6839442ff7
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-08 20:30:11 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
-          Darcy Weipert
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Darcy-Weipert/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
+            Darcy Weipert
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Darcy-Weipert/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
